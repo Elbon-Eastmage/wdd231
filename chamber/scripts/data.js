8f9b940b3b2a76a5ef1c71b4563f2bc823a3874e
Elbon Eastmage <elbon.eastmage@gmail.com>
2026-09-12 17:15:32 -0600

    Completed the Chamber Directory assignment.

--- chamber/scripts/data.js
+++ chamber/scripts/data.js
@@ -1,24 +1,60 @@
-const cards = document.querySelector(".cards");
+const entries = document.querySelector("#entries");
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+let isGrid = true;
+
+gridButton.addEventListener("click", () => {
+    isGrid = true;
+    getMembers();
+});
+
+listButton.addEventListener("click", () => {
+    isGrid = false;
+    getMembers();
+})
+
+getMembers();
 
 async function getMembers() {
     const data = await fetch("https://elbon-eastmage.github.io/wdd231/chamber/data/members.json");
-    const members = await data.json().companies;
-    displayMembers(members);
+    const members = await data.json();
+    displayMembers(members.companies);
 }
 
 function displayMembers(members) {
+    entries.innerHTML = "";
+
+    if (isGrid) {
+        entries.classList.add("cards");
+        entries.classList.remove("list");
+    }
+    else {
+        entries.classList.remove("cards");
+        entries.classList.add("list");
+    }
+
     for (const member of members) {
-        const card = document.createElement("section");
-        card.classList.add("card");
-        card.innerHTML =
-            `<h2>${member.name}</h2>
-            <img src="${imageFileName}" loading="lazy" alt="${member.name}" height="80" width="80">
-            <p>
-            <span class="emphasize">Address: </span>${member.address}<br>"
-            <span class="emphasize">Phone: </span>${member.phone}<br>"
-            <span class="emphasize">URL: </span>${member.url}"
-            </p>`;
-        
-        cards.appendChild(card);
+        const entry = document.createElement("section");
+        entry.classList.add("entry");
+
+        if (isGrid) {
+            entry.innerHTML =
+                `<h2>${member.name}</h2>
+                <img src="${member.imageFileName}" loading="lazy" alt="${member.name}" height="80" width="80">
+                <p>
+                <span class="emphasize">Address: </span>${member.address}<br>
+                <span class="emphasize">Phone: </span>${member.phone}<br>
+                <span class="emphasize">URL: </span>${member.url}
+                </p>`;
+        }
+        else {
+            entry.innerHTML =
+                `<h2>${member.name}</h2>
+                <p>${member.address}</p>
+                <p>${member.phone}</p>
+                <p>${member.url}</p>`;
+        }
+
+        entries.appendChild(entry);
     }
 }
